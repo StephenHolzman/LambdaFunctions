425664f8e0f8886240916ccfc8585b7e8bf24825
StephenHolzman <stephen@holzmansmith.com>
2016-03-15 14:56:08 -0400

    Add comments

--- HuffPostNationalEstimates/exports.js
+++ HuffPostNationalEstimates/exports.js
@@ -1,52 +1,82 @@
-
+//Load necessary javascript libraries
 var request = require('request');
 var async = require('async');
 var AWS = require('aws-sdk');
 var json2csv = require('json2csv');
 
+/* The handler function is the main function wrapper.
+ * Event is an object passed to the function by Lambda when it is called.
+ * Because this is scheduled, it is not dependent on any of the event properties.
+ * Context is an object passed by Lambda that contains methods for terminating the function when done.
+ */
+
 exports.handler = function(event, context){
+
+    /* URL is the API call
+     * Keyname is the filename
+     * Choices are party candidates I'm interested in.
+     */
+
     var requestUrls = [
         {
             "url":"http://elections.huffingtonpost.com/pollster/api/charts/2016-national-gop-primary.json",
             "keyname":"election/data/gop_national_estimates.csv",
             "choices":["Trump","Cruz","Rubio","Kasich","Carson","Bush","Christie","Rand Paul","Fiorina","Jindal"]
-        },{
+        },
+        {
             "url":"http://elections.huffingtonpost.com/pollster/api/charts/2016-national-democratic-primary.json",
             "keyname":"election/data/dem_national_estimates.csv",
             "choices":["Clinton","Sanders"]
-        }]
+        };
+    ];
 
-    var partiesdone = 0;
+    /* Define function that uploads cleaned CSV to S3 that will be called later in request callback
+     * Uploadobject is the body returned by the API request
+     * Keyname is the keyname property from the requestUrls item.
+     * Choices is the choices property from the requestUrls item.
+     */
 
     var uploadS3 = function(uploadobject,keyname,choices){
+
+
         var s3 = new AWS.S3();
         var prepJSONforCSV = [];
-        var fields = choices;
-        fields.push("date");
 
-        //Build JSON that works with json2csv
+        /* Build JSON that works with json2csv from uploadobject
+         */
+
         uploadobject.estimates_by_date.forEach(function(d,i){
-            //console.log(d);
+
             obj = new Object();
             obj.date = d.date;
             for(c in choices){
                 for(value in d.estimates){
                     if(d.estimates[value].choice === choices[c]){
                         obj[choices[c]] = d.estimates[value].value;
-                    }       
-                }
-                
-            }
+                    };       
+                };             
+            };
             prepJSONforCSV.push(obj);
 
         });
 
-        //convert json to csv
+        /* Convert json to csv
+         * Fields are csv headers
+         */
+
+        var fields = choices;
+        fields.push("date");
+
         json2csv({ data: prepJSONforCSV, fields: fields }, function(err, csv) {
 
             if (err) console.log(err);
 
-            //Upload csv to S3
+            /* AWS access.
+             * Credentials are taken care of by Lambda.
+             * Creates a bucket for files if it doesn't exist.
+             * uploads object with keyname as filename.
+             */
+
             s3.createBucket({Bucket: 'chartsoncharts.com'}, function() {
 
                 var params = {Bucket: 'chartsoncharts.com', Key: keyname, Body: csv}
@@ -55,19 +85,25 @@ exports.handler = function(event, context){
 
                     if (err){
                         console.log(err);
+                        //If there's an error, context fail let's Lambda know you failed and ends the function.
                         context.fail();
                     }else{
                         console.log("Successfully uploaded data to S3!");
                         partiesdone++;
                         if(partiesdone===2){
+                            //If the expected number of objects have been uploaded, context succeed let's Lambda know you succeeded and ends the function.
                             context.succeed(); 
                         }       
                     }
                 });
             });
-        });
-    };
 
+        });
+
+    };
+    /*Actually run the code.
+     *I chose to put context.succeed() in putObject function, but could just as easily be in the async.foreach callback here.
+     */
     var req = async.forEach(requestUrls, function(item,callback){
         
         request({
